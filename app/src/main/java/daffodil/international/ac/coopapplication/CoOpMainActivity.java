4ba5b7fb31c8b0a3538ce3c1b3358fa9296f1e79
Masum Islam <islammasum34@gmail.com>
2017-06-03 12:40:19 +0600

    Project created

---
+++ app/src/main/java/daffodil/international/ac/coopapplication/CoOpMainActivity.java
@@ -0,0 +1,13 @@
+package daffodil.international.ac.coopapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoOpMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_co_op_main);
+    }
+}
