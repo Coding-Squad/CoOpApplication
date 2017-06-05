c79ef183cf115d29e4e4996d592366095207387f
Masum Islam <islammasum34@gmail.com>
2017-06-05 23:55:53 +0600

    Updated

--- app/src/main/java/daffodil/international/ac/coopapplication/SignUpAsActivity.java
+++ app/src/main/java/daffodil/international/ac/coopapplication/SignUpAsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class SignUpAsActivity extends AppCompatActivity {
 
@@ -11,6 +12,12 @@ public class SignUpAsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_as);
+
+        // Scrollbar activation for signup as linear layout.
+
+       /* LinearLayout signUpAsLinearLayout = (LinearLayout) findViewById(R.id.signupas_linearLayout);
+        signUpAsLinearLayout.addView();*/
+
     }
 
     public void goToStudentSignUpPage(View view) {
