7572046a7fa227b85926cd38330bdf7c8932c27a
Masum-Islam <islamasum@gmail.com>
2017-06-05 16:29:44 +0600

    Some UI added.

--- app/src/main/java/daffodil/international/ac/coopapplication/CoOpMainActivity.java
+++ app/src/main/java/daffodil/international/ac/coopapplication/CoOpMainActivity.java
@@ -14,18 +14,26 @@ public class CoOpMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_co_op_main);
 
-        final Button button =(Button) findViewById(R.id.student_signup_button);
+       /* final Button button =(Button) findViewById(R.id.student_signup_button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Perform action on click
                 Log.d(TAG, "onClick: starts");
          //       Toast.makeText(CoOpMainActivity.this, "Sign up clicked ", Toast.LENGTH_SHORT).show();
 
-                Intent intent = new Intent(CoOpMainActivity.this,StudentSignUpActivity.class);
-                CoOpMainActivity.this.startActivity(intent);
+              *//*  Intent intent = new Intent(CoOpMainActivity.this,StudentSignUpActivity.class);
+                CoOpMainActivity.this.startActivity(intent);*//*
 
             }
         });
 
+
+*/
+    }
+
+    public void goToSignUpAsPage(View view) {
+        Intent signUpPageIntent = new Intent
+                (this, SignUpAsActivity.class);
+        startActivity(signUpPageIntent);
     }
 }
---
+++ app/src/main/java/daffodil/international/ac/coopapplication/SignUpAsActivity.java
@@ -0,0 +1,21 @@
+package daffodil.international.ac.coopapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpAsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_as);
+    }
+
+    public void goToStudentSignUpPage(View view) {
+        Intent studentSignUpIntent = new Intent(this, StudentSignUpActivity.class);
+        startActivity(studentSignUpIntent);
+
+    }
+}
