9a3817e5d61a2f05725473beab58091029270e6c
Pranto20018 <pranto20018.khan@gmail.com>
2017-06-05 14:31:49 +0600

    Student SignUp form Degin

---
+++ app/src/main/java/daffodil/international/ac/coopapplication/StudentSignUpActivity.java
@@ -0,0 +1,13 @@
+package daffodil.international.ac.coopapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_sign_up);
+    }
+}
