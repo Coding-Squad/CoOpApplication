9ae83941cc2caf391e574770605399dc6cbd977f
Masum-Islam <islamasum@gmail.com>
2017-08-16 16:37:57 +0600

    Date Picker bug fixed.

---
+++ app/src/main/java/daffodil/international/ac/coopapplication/daffodil/international/ac/coopapplication/datepicker/DatePickerFragment.java
@@ -0,0 +1,58 @@
+package daffodil.international.ac.coopapplication.daffodil.international.ac.coopapplication.datepicker;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.icu.util.Calendar;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import daffodil.international.ac.coopapplication.R;
+
+/**
+ * Created by Programmer on 13-Aug-17.
+ */
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        //Use the current date as the default date in the date picker
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        //Create a new DatePickerDialog instance and return it
+        /*
+            DatePickerDialog Public Constructors - Here we uses first one
+            public DatePickerDialog (Context context, DatePickerDialog.OnDateSetListener callBack, int year, int monthOfYear, int dayOfMonth)
+            public DatePickerDialog (Context context, int theme, DatePickerDialog.OnDateSetListener listener, int year, int monthOfYear, int dayOfMonth)
+         */
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+
+    }
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        //Do something with the date chosen by the user
+        EditText datePickerButton = (EditText) getActivity().findViewById(R.id.date_of_birth);
+        /*tv.setText("Date changed...");
+        tv.setText(tv.getText() + "\nYear: " + year);
+        tv.setText(tv.getText() + "\nMonth: " + month);
+        tv.setText(tv.getText() + "\nDay of Month: " + day);*/
+        //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        //String date = sdf.format(new Date());
+        String stringOfDate = year + "/" + month + "/" + day;
+        datePickerButton.setText(datePickerButton.getText() + "\n\n " + stringOfDate);
+        System.out.println(datePickerButton.getText().toString());
+    }
+
+
+}
